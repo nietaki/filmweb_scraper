b8dcb52a648950c2fba7cb1482429154680f890d
Jacek Królikowski <nietaki@gmail.com>
2015-01-10 13:59:16 +0000

    exact string matching experiment looking good

--- src/main/scala/Matcher.scala
+++ src/main/scala/Matcher.scala
@@ -3,5 +3,30 @@
  */
 object Matcher {
   lazy val imdbFilms = Imdb.parseRatingsWithIds("res/imdb/ratings.list").toArray
-  lazy val filmwebFilms = Filmweb.readAllFromCsv("res/fil")
+  lazy val filmwebFilms = Filmweb.readAllFromCsv("res/filmweb.csv")
+
+  lazy val imdbYearly = imdbFilms.groupBy(_.year)
+  lazy val filmwebYearly = imdbFilms.groupBy(_.year)
+
+  def getExactMatches() = {
+    filmwebFilms.foreach {ff =>
+      val imdbOfThisYear = imdbYearly.get(ff.year)
+      val found = imdbOfThisYear match {
+        case None => None//no films in this year
+        case Some(arr) => {
+          arr.find {imdbFilm =>
+            val imdbTitle = Levenshtein.normalizeString(imdbFilm.title)
+            val filmbwebTitle = Levenshtein.normalizeString(ff.title)
+            val filmbwebOrigTitle = Levenshtein.normalizeString(ff.originalTitle)
+            (imdbTitle == filmbwebTitle) || (imdbTitle == filmbwebOrigTitle)
+          }
+        }
+      }
+      val msg = found match {
+        case Some(imdbFound) => s"${imdbFound.title} - imdb: ${imdbFound.rating}, filmweb: ${ff.rating}"
+        case None => s"no match found for ${ff.title}"
+      }
+      println(msg)
+    }
+  }
 }
--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -3,11 +3,16 @@
  */
 object Runner {
   def main(args: Array[String]): Unit = {
-    Filmweb.scrape()
+    //Filmweb.scrape()
 
     //Imdb.parseRatings("res/imdb/ratings.list").foreach(println(_))
     //println(Imdb.parseRatings("res/imdb/ratings.list").toList.length)
     //Imdb.getFileLines("res/imdb/ratings.list").foreach(println(_))
-    Imdb.parseRatingsWithIds("res/imdb/ratings.list").foreach(println(_))
+    //Imdb.parseRatingsWithIds("res/imdb/ratings.list").foreach(println(_))
+
+    //Matcher.filmwebFilms.take(10).foreach(println(_))
+    //Matcher.imdbFilms.take(10).foreach(println(_))
+
+    Matcher.getExactMatches
   }
 }
